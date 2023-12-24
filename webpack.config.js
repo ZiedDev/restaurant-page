c042a60f80e555c364bb2569685f15b92b8a12b8
Zied <73042811+ZiedDev@users.noreply.github.com>
2023-12-24 19:10:30 +0200

    make docs folder for github pages

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
+    mode: 'production',
+    // devtool: 'inline-source-map',
     entry: [
         './src/js/index.js',
         './src/js/domElements.js',
